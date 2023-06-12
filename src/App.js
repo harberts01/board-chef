d1ba1bdb52d95fab7e3da88d1e5f1ed8113fe691
Philip Harberts <phil@Philips-MacBook-Pro.local>
2023-06-11 23:14:47 -0500

    Updating Start Page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <StartPage />
-      <HomePage />
+      {/* <HomePage /> */}
     </div>
   );
 }
--- src/components/LoginCard.js
+++ src/components/LoginCard.js
@@ -6,25 +6,29 @@ import Logo from '../images/Logo.jpg'
 import { Stack }  from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 import { Card } from 'react-bootstrap';
+import Background from '../images/cutting-board.jpg';
 
 
 const LoginCard = () => {
   return (
-    <Container>
-      <Row style={{ height: '100vh' }}>
-        <Col className="d-flex align-self-center justify-content-center">
-          <Card style={{ width: '18rem', height: '25rem' }}>
-            <Card.Img variant="top" src={Logo} />
-              <Card.Body className='col-8 d-flex align-self-center'>
-                <Stack gap={2} className="justify-content-center">
-                    <Button id='LogInBtn' onClick="./HomePage.js" variant="primary">Log In</Button>
-                        <Button id='SignUpBtn' variant="primary">Sign Up</Button>
-               </Stack>
-            </Card.Body>      
-          </Card>   
-        </Col>
-      </Row>
+    <div className="d-flex align-self-center justify-content-center">
+       <Container>
+        <Row id="startPageBg" style={{ backgroundImage: `url(${Background})`}}>
+          <Col className="d-flex align-self-center justify-content-center">
+            <Card style={{ width: '18rem', height: '20rem', padding: '20px' }}>
+              <Card.Img variant="top" src={Logo} />
+                <Card.Body className='col-8 d-flex align-self-center'>
+                  <Stack gap={2} className="justify-content-center">
+                      <Button id='LogInBtn' onClick="./HomePage.js" variant="primary" size='lg'>Login</Button>
+                          <Button id='SignUpBtn' variant="primary" size='lg'>Sign Up</Button>
+                </Stack>
+              </Card.Body>      
+            </Card>   
+          </Col>
+        </Row>
     </Container>
+    </div>
+   
   )
 }
 
